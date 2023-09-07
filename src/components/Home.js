283944e6c595f645c5a666a5b7f645236575f0f7
mahmud-alam <official.mahmud23@gmail.com>
2023-09-07 17:49:00 +0600

    store user input data into localStorage

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,15 +10,15 @@ const Home = () => {
         email:"",
         password:""
     })
-    console.log(inpVal);
+    //console.log(inpVal);
 
     const [data, setData] = useState([])
     
     const getData = (e) => {
-        console.log(e.target.value);
+        //console.log(e.target.value);
         
         const {value, name} = e.target;
-        console.log(value, name);
+        //console.log(value, name);
 
         setInpVal(()=>{
             return{
@@ -27,6 +27,34 @@ const Home = () => {
             }
         })
 
+    }
+
+    const setlocalStorage = (e) => {
+        e.preventDefault();
+
+        console.log(inpVal);
+
+        const {username, name, email, password} = inpVal;
+
+        if(username === ""){
+            alert('Username is required!')
+        }else if(username.length < 4){
+            alert('Username should be at least 4 characters long')
+        }else if(name === ""){
+            alert('Name is required!')
+        }else if(email === ""){
+            alert('Email is required!')
+        }else if(!email.includes("@")){
+            alert('Please Enter valid Email Address!')
+        }else if(password === ""){
+            alert('Password is required!')
+        }else if(password.length < 6){
+            alert('Password should be at least 6 characters long')
+        }else{
+            localStorage.setItem("userData",JSON.stringify([...data,inpVal]));
+
+        }
+        
     }
 
 
@@ -53,7 +81,7 @@ const Home = () => {
                 <Form.Group className="mb-3 col-lg-8 col-md-8" controlId="formBasicPassword">
                   <Form.Control name="password" type="password" onChange={getData} placeholder="Password" />
                 </Form.Group>
-                <Button className={`col-lg-8 col-md-8 ${HomeCSS.btn}`} variant="primary" type="submit">
+                <Button className={`col-lg-8 col-md-8 ${HomeCSS.btn}`} onClick={setlocalStorage} variant="primary" type="submit">
                   Submit
                 </Button>
               </Form>
