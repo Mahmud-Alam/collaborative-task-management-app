35e24861e3c09bcf863c355905921e1e2869f765
mahmud-alam <official.mahmud23@gmail.com>
2023-09-07 17:11:31 +0600

    Header css module added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
+import HeaderCSS from './Header.module.css';
+
 
 const Header = () => {
   return (
