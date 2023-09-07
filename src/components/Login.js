c71766c518458b57821b155bb66e47bc2fba2b71
mahmud-alam <official.mahmud23@gmail.com>
2023-09-07 19:51:17 +0600

    Login component added

---
+++ src/components/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import HomeCSS from './Home.module.css';
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+
+const Login = () => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col lg={6} md={6} className="mt-5">
+            <h3 className="text-center col-lg-8 col-md-8">Sign In</h3>
+            <div>
+              <Form>
+                <Form.Group className="mb-3 col-lg-8 col-md-8" controlId="formBasicEmail">
+                  <Form.Control name="email" type="email" onChange={getData} placeholder="Email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3 col-lg-8 col-md-8" controlId="formBasicPassword">
+                  <Form.Control name="password" type="password" onChange={getData} placeholder="Password" />
+                </Form.Group>
+                <Button className={`col-lg-8 col-md-8 ${HomeCSS.btn}`} onClick={setlocalStorage} variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+              <p className="mt-3">Already Have an Account <span>Signin</span></p>
+            </div>
+          </Col>
+          <Col lg={6} md={6} className="mt-3 align-self-center">
+            <div>
+                <img className={`${HomeCSS.homeImg}`} src="./homeImg.jpg" alt=""/>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default Login
