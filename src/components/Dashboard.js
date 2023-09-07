2ed8e901333febcfc18d2cdd321d822faff8df57
mahmud-alam <official.mahmud23@gmail.com>
2023-09-07 22:27:26 +0600

    update dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Dashboard = () => {
+
+    const getUser = localStorage.getItem("user_login")
+    const user = JSON.parse(getUser)
+
   return (
-    <div>Dashboard</div>
+    <div className='mt-5 pt-5 text-center'>
+        <h1>Welcome, {`${user[0].name}`}</h1>
+    </div>
   )
 }
 
