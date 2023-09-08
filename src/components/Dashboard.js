922751e79e4a719cba275e7a7ba46afc394090ff
mahmud-alam <official.mahmud23@gmail.com>
2023-09-08 21:46:13 +0600

    [ADD] user table to dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,24 +1,56 @@
-import React, { useState } from 'react'
-import { Button } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { Button, Col, Container, Row, Table } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import HomeCSS from './Home.module.css'
 const Dashboard = () => {
+  const loginUser = JSON.parse(localStorage.getItem("user_login"))[0];
+  const allUserData = JSON.parse(localStorage.getItem("userData"))[0];
 
-    const getUser = localStorage.getItem("user_login")
-    const user = JSON.parse(getUser)
+  const redirect = useNavigate();
 
-    const history = useNavigate()
-
-    const userLogout = () => {
-      localStorage.removeItem("user_login")
-      history('/login');
-    }
+  const userLogout = () => {
+    localStorage.removeItem("user_login");
+    redirect("/login");
+  };
 
   return (
-    <div className='mt-5 pt-5 text-center'>
-        <h1>Welcome, {`${user[0].name}`}</h1>
-        <Button onClick={userLogout}>Logout</Button>
-    </div>
-  )
-}
+    <>
+      <div className="mt-3 pt-3 text-center">
+        <Container>
+          <Row>
+            <Col>
+              <h1>Welcome, {`${loginUser.name}`}</h1>
+              <Button className={`mb-5 ${HomeCSS.btn}`} onClick={userLogout}>
+                Logout
+              </Button>
+            </Col>
+            <Col>
+              <Table responsive="sm">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Username</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {allUserData.map((user, i) => (
+                    <tr key={i}>
+                      <td>{i + 1}</td>
+                      <td>{user.username}</td>
+                      <td>{user.name}</td>
+                      <td>{user.email}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
